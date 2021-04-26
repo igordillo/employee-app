15a1c1bc97b5657c3faf9110cc0cc1adeda1378b
Isaac Gordillo <isaacggomez@gmail.com>
2021-04-26 10:13:25 +0200

    fix capitalize util method

--- src/app/core/utils.ts
+++ src/app/core/utils.ts
@@ -1,2 +1,7 @@
-export const capitalizeText = (text: string) =>
-  text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+export const capitalizeText = (text: string) => {
+  const arrayText = text
+    .split(' ')
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase());
+
+  return arrayText.join(' ');
+};
