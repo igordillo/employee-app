5faa6fb491c40f333687288c4b09826a4bdfab4e
igordillo <isaac.gordillo@twtspain.com>
2021-04-26 09:31:19 +0200

    create some unit tests

--- src/app/core/services/employee.service.spec.ts
+++ src/app/core/services/employee.service.spec.ts
@@ -1,20 +1,67 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { EmployeeService } from './employee.service';
+import { Employee } from '../models/employee.model';
+import { of } from 'rxjs';
+import { EMPLOYEES_API_ENDPOINT } from '../constants';
+import { environment } from 'src/environments/environment';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('EmployeeService', () => {
+  let httpClientSpy: { get: jasmine.Spy };
   let service: EmployeeService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       providers: [EmployeeService],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
     });
-    service = TestBed.inject(EmployeeService);
+
+    httpMock = TestBed.inject(HttpTestingController);
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+    service = new EmployeeService(httpClientSpy as any)
+  });
+
+  afterAll(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should not immediately connect to the server', () => {
+    httpMock.expectNone({});
+  });
+
+  it('getAllEmployes', () => {
+    const emplooyees: Employee[] = [
+      {
+        "id": 1,
+        "name": "Isaac",
+        "surname": "Gordillo gómezasd",
+        "workPosition": "full-stack developer",
+        "dateOfBirth": "1990-12-20T23:00:00.000Z"
+      },
+      {
+        "id": 2,
+        "name": "Sara",
+        "surname": "Lopez",
+        "workPosition": "full-stack developer",
+        "dateOfBirth": "2021-04-28T22:00:00.000Z"
+      },
+    ];
+
+    httpClientSpy.get.and.returnValue(of(emplooyees));
+
+    service.getAllEmployees();
+
+    service.employeesStore$.subscribe(resp => {
+      expect(resp).toEqual(emplooyees);
+    })
+
+  })
 });
--- src/app/employee-list/employee-list.component.spec.ts
+++ src/app/employee-list/employee-list.component.spec.ts
@@ -1,33 +1,77 @@
+import { HttpClient } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatDialog, MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatSelectModule } from '@angular/material/select';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { Employee } from '@core/models/employee.model';
 import { EmployeeService } from '@core/services/employee.service';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { Observable, of } from 'rxjs';
 
 import { EmployeeListComponent } from './employee-list.component';
+import { EmployeeDialogData, EmployeeFormDialogComponent } from '../employee-form-dialog/employee-form-dialog.component';
+import { formatDate } from '@angular/common';
+import { DEFAULT_LANG, FORMAT_DATE } from '@core/constants';
+import { MaterialModule } from '../shared/material/material.module';
+
+
+let translations: any = { "CARDS_TITLE": "This is a test" };
+
+class FakeLoader implements TranslateLoader {
+  getTranslation(lang: string): Observable<any> {
+    return of(translations);
+  }
+}
+
+export function httpTranslateLoader(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
+
+const mockDialogRef = {
+  close: jasmine.createSpy('close'),
+};
+
+const dataDialog: EmployeeDialogData = {
+  action: 'Add',
+  employee: {
+    name: 'name',
+    surname: 'surname',
+    workPosition: '',
+    dateOfBirth: formatDate(new Date(), FORMAT_DATE, DEFAULT_LANG),
+  },
+};
 
 describe('EmployeeListComponent', () => {
   let component: EmployeeListComponent;
   let fixture: ComponentFixture<EmployeeListComponent>;
+  let employeeService: EmployeeService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [EmployeeListComponent],
+      declarations: [EmployeeListComponent, EmployeeFormDialogComponent],
       imports: [
         HttpClientTestingModule,
         FormsModule,
         ReactiveFormsModule,
-        MatDialogModule,
-        MatSelectModule,
-        TranslateModule.forRoot(),
+        MaterialModule,
+        TranslateModule.forRoot({
+          defaultLanguage: 'en',
+          loader: {
+            provide: TranslateLoader,
+            useFactory: httpTranslateLoader,
+            deps: [HttpClient],
+          },
+        }),
         ToastrModule.forRoot(),
+        NoopAnimationsModule
       ],
-      providers: [EmployeeService],
+      providers: [EmployeeService, TranslateService],
       schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
     }).compileComponents();
   });
@@ -36,9 +80,64 @@ describe('EmployeeListComponent', () => {
     fixture = TestBed.createComponent(EmployeeListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    employeeService = fixture.debugElement.injector.get(EmployeeService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('refreshEmployees', () => {
+    const employees: Employee[] = [
+      {
+        "id": 1,
+        "name": "Isaac",
+        "surname": "Gordillo gómezasd",
+        "workPosition": "full-stack developer",
+        "dateOfBirth": "1990-12-20T23:00:00.000Z"
+      },
+      {
+        "id": 2,
+        "name": "Sara",
+        "surname": "Lopez",
+        "workPosition": "full-stack developer",
+        "dateOfBirth": "2021-04-28T22:00:00.000Z"
+      },
+    ];
+
+    component.employees = employees;
+
+    component.pageSize = 1;
+    component.refreshEmployees();
+
+    expect(component.employeesDataTable.length).toEqual(1);
+  })
+
+  it('onRefreshButton', () => {
+    const spy = spyOn(employeeService, 'getAllEmployees').and.callFake(() => null);
+
+    component.onRefreshButton();
+
+    expect(spy).toHaveBeenCalled();
+  })
+
+  it('onAddButon', () => {
+    const spy = spyOn(component, 'onAddButon').and.callThrough();
+    component.onAddButon();
+    expect(spy).toHaveBeenCalled();
+
+  })
+
+  it('onEditButton', () => {
+    const employee: Employee = {
+      "id": 1,
+      "name": "Isaac",
+      "surname": "Gordillo gómezasd",
+      "workPosition": "full-stack developer",
+      "dateOfBirth": "1990-12-20T23:00:00.000Z"
+    };
+    const spy = spyOn(component, 'onEditButton').and.callThrough();
+    component.onEditButton(employee);
+    expect(spy).toHaveBeenCalled();
+  })
 });
