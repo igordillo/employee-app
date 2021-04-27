ba9a02fb043ba0281e825e579277735c5f43a355
igordillo <isaac.gordillo@twtspain.com>
2021-04-27 09:15:03 +0200

    performance improving

--- src/app/employee-form-dialog/employee-form-dialog.component.ts
+++ src/app/employee-form-dialog/employee-form-dialog.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Inject, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Employee } from '@core/models/employee.model';
@@ -36,23 +36,36 @@ export class EmployeeFormDialogComponent implements OnInit {
     private readonly toastr: ToastrService,
     @Inject(MAT_DIALOG_DATA) public data: EmployeeDialogData,
     private readonly translate: TranslateService,
-    public readonly validateFormService: ValidateFormService,
-
-  ) {}
+    public readonly validateFormService: ValidateFormService
+  ) {
+    this.form = this.fb.group({
+      name: ['', [Validators.required, Validators.pattern(namesRegex)]],
+      surname: ['', [Validators.required, Validators.pattern(namesRegex)]],
+      dateOfBirth: [
+        '',
+        Validators.required,
+      ],
+      workPosition: ['', Validators.required],
+    });
+  }
 
   ngOnInit(): void {
     this.workPositionsService
       .getAllWorkPositions()
-      .subscribe((positions: string[]) => (this.workPositions = positions));
+      .subscribe((positions: string[]) => {
+        this.workPositions = positions
+        this.resetForm();
+      });
 
-    this.form = this.fb.group({
-      name: [this.data.employee.name, [Validators.required, Validators.pattern(namesRegex)]],
-      surname: [this.data.employee.surname, [Validators.required, Validators.pattern(namesRegex)]],
-      dateOfBirth: [
-        new Date(this.data.employee.dateOfBirth),
-        Validators.required,
-      ],
-      workPosition: [this.data.employee.workPosition, Validators.required],
+
+  }
+
+  resetForm() {
+    this.form.reset({
+      name: this.data.employee.name,
+      surname: this.data.employee.surname,
+      dateOfBirth: new Date(this.data.employee.dateOfBirth),
+      workPosition: this.data.employee.workPosition
     });
   }
 
